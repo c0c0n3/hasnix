546aa881dd22de7efa453bb3f9a7183ca604080f
Andrea Falconi <andrea.falconi@gmail.com>
2021-03-13 11:03:53 +0100

    make haskell drvs overridable when assembling overlay.

--- build/lib/cabal-pkgs.nix
+++ build/lib/cabal-pkgs.nix
@@ -5,16 +5,59 @@
 { pkgs }:
 
 let
-  inherit (pkgs.lib.filesystem) haskellPathsInDir;
   inherit (pkgs.lib.attrsets) mapAttrs;
+  inherit (pkgs.lib.filesystem) haskellPathsInDir;
+  inherit (pkgs.haskell.lib) doBenchmark dontBenchmark doCheck dontCheck
+    doHaddock dontHaddock enableCabalFlag;
   stockCallCabal2nix = pkgs.haskellPackages.callCabal2nix;
 in rec {
+
+    # Call Cabal2Nix to build a derivation out of a Cabal file.
+    # Use the Cabal file `pkgDirAbsPath/pkgName.cabal` and give the derivation
+    # a name of `pkgName`. Accept optional arguments to tweak the derivation,
+    # see below for the details.
+    makeNixDrv = { # Use a specific `cabal2nix`.
+                   callCabal2nix ? stockCallCabal2nix
+                   # Run tests?
+                 , withTests ? false
+                   # Enable benchmarks?
+                 , withBenches ? false
+                   # Build Haddock documentation?
+                 , withDocs ? false
+                   # List of flags to pass to Cabal. We turn on each of
+                   # them when building the Haskell source. Don't add the
+                   # `-f`, e.g. use "release" instead of "-f release".
+                   , withFlags ? []
+                 , ...
+                 }:
+                 pkgName:
+                 pkgDirAbsPath:
+      let
+        enableFlag = flag: drv: enableCabalFlag drv flag;
+
+        fs = (if withTests then [ doCheck ] else [ dontCheck ])
+          ++ (if withBenches then [ doBenchmark ] else [ dontBenchmark ])
+          ++ (if withDocs then [ doHaddock ] else [ dontHaddock ])
+          ++ (map enableFlag withFlags);
+
+        bareDrv = callCabal2nix pkgName pkgDirAbsPath {};
+      in
+        builtins.foldl' (drv: f: f drv) bareDrv fs;    # (*)
+    # NOTE
+    # It doesn't look like we're able to override attrs at this stage.
+    # E.g.
+    #   bareDrv.overrideAttrs (oldAttrs: rec { doBenchmark = true; });
+    # would've been easier but it has no effect, the derivation won't
+    # include benchmark deps if there's some in the Cabal file. Also,
+    # `bareDrv.override` isn't there so Nix pukes if you call it.
 
     # Find all Cabal packages underneath a given base directory and convert
     # them to Nix derivations. Take a Cabal package to be any sub-directory
     # `s` with an `s.cabal` file in it---i.e. the Cabal file name (without
     # extension) is the same as the enclosing directory's name. Name the
     # corresponding Nix derivation `s`.
+    # Also accept any of the arguments of `makeNixDrv` to tweak derivations,
+    # e.g. don't build docs. (Notice these options get applied to all drvs.)
     #
     # Example.
     #
@@ -32,14 +75,11 @@ in rec {
     # toNixDrvs { baseDir = /my/project/components; }
     # => { pkg1 = <pkg1 drv>; pkg3 = <pkg3 drv>; }
     #
-    toNixDrvs = { baseDir, callCabal2nix ? stockCallCabal2nix }:
-      let
-        toDrv = dirName: absPath: callCabal2nix dirName absPath {};
-      in
-        mapAttrs toDrv (haskellPathsInDir baseDir);    # (*)
-      # NOTE
-      # Have a look at haskellPathsInDir to see how it finds Cabal package
-      # dirs. It returns a set { d1 = /path/to/d1; d2 = /path/to/d2; ...}.
+    toNixDrvs = { baseDir, ... }@args:
+      mapAttrs (makeNixDrv args) (haskellPathsInDir baseDir);  # (*)
+    # NOTE
+    # Have a look at haskellPathsInDir to see how it finds Cabal package
+    # dirs. It returns a set { d1 = /path/to/d1; d2 = /path/to/d2; ...}.
 
     # Turn a list of Haskell derivations into a set containing an attribute
     # for each input derivation and where each attribute name is the package
